01b219dbaddef6afe0b5cbf145227d6a99fefaa6
RedTosS39 <redtoss@gmail.com>
2021-07-20 19:04:27 +0300

    Добавьте файлы проекта.

---
+++ ProjectPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
